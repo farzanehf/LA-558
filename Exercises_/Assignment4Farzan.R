fe66a5a3266f9c003857a3e711e5cfe35eff582d
farzanehf <47862526+farzanehf@users.noreply.github.com>
2023-03-23 12:00:14 -0500

    ad

---
+++ Exercises_/Assignment4Farzan.R
@@ -0,0 +1,103 @@
+#Create a Leaflet page in R that includes at least 20 markers. The markers should include a popup or a label, and can be a marker, circle or a circle Marker. Due one of the following 
+
+#Create icons as described in https://rstudio.github.io/leaflet/markers.html  
+
+#Add in a control to select from one of five basemaps – do not just copy my code, you can select a few of your own tiles 
+
+#Add the ability to cluster the points when zoomed out 
+
+##create a Leaflet page in R that includes at least 20 markers
+
+library(leaflet)
+m <- leaflet() %>%
+  setView(lng = -93.648, lat = 42.0267, zoom = 15)  # set the initial view on Iowa State University
+
+# add 20 markers
+locations <- data.frame(
+  name = paste0("Marker ", 1:20),
+  lon = runif(20, -93.662, -93.634),
+  lat = runif(20, 42.009, 42.042)
+)
+m <- addMarkers(map = m, data = locations, ~lon, ~lat, popup = ~name,label = ~name)
+m
+# create a custom icon
+myIcon <- makeIcon(iconUrl = "https://leafletjs.com/examples/custom-icons/leaf-red.png",
+                   iconWidth = 38, iconHeight = 95)
+
+# add 20 markers with custom icon
+locations <- data.frame(
+  name = paste0("Marker ", 1:20),
+  lon = runif(20, -93.662, -93.634),
+  lat = runif(20, 42.009, 42.042)
+)
+m <- addMarkers(map = m, data = locations, ~lon, ~lat, popup = ~name, icon = myIcon)
+
+# display the map
+m 
+
+m <- leaflet() %>%
+  setView(lng = -93.648, lat = 42.0267, zoom = 15)  # set the initial view on Iowa State University
+
+# add 20 markers with custom icon
+locations <- data.frame(
+  name = paste0("Marker ", 1:20),
+  lon = runif(20, -93.662, -93.634),
+  lat = runif(20, 42.009, 42.042)
+)
+myIcon <- makeIcon(iconUrl = "https://leafletjs.com/examples/custom-icons/leaf-red.png",
+                   iconWidth = 38, iconHeight = 95)
+m <- addMarkers(map = m, data = locations, ~lon, ~lat, popup = ~name, icon = myIcon)
+m
+# add basemap layers and control
+m <- addTiles(map = m, urlTemplate = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+              attribution = "Map data © <a href='https://openstreetmap.org'>OpenStreetMap</a> contributors")
+m <- addProviderTiles(map = m, providers$Esri.WorldTopoMap, group = "Esri")
+m <- addProviderTiles(map = m, providers$Esri.WorldImagery, group = "Esri")
+m <- addProviderTiles(map = m, providers$Stamen.Toner, group = "Stamen")
+m <- addProviderTiles(map = m, providers$Stamen.Terrain, group = "Stamen")
+m <- addLayersControl(map = m, baseGroups = c("OpenStreetMap", "Esri", "Stamen"),
+                      options = layersControlOptions(collapsed = FALSE))
+
+# display the map
+m
+
+
+# Create a Leaflet page in R that includes a chloropleth. Import your shapefile and set the projection as demonstrated. Due two of the following 
+
+# Add a Popup or a label 
+
+# Add a Legend 
+
+# Add an additional layers containing lines or markers. 
+
+
+library(leaflet)
+
+# create a map
+m <- leaflet() %>%
+  setView(lng = -93.648, lat = 42.0267, zoom = 15)  # set the initial view on Iowa State University
+
+# add 20 markers with custom icon
+locations <- data.frame(
+  name = paste0("Marker ", 1:20),
+  lon = runif(20, -93.662, -93.634),
+  lat = runif(20, 42.009, 42.042)
+)
+myIcon <- makeIcon(iconUrl = "https://leafletjs.com/examples/custom-icons/leaf-red.png",
+                   iconWidth = 38, iconHeight = 95)
+m <- addMarkers(map = m, data = locations, ~lon, ~lat, popup = ~name, icon = myIcon,
+                clusterOptions = markerClusterOptions())
+
+# add basemap layers and control
+m <- addTiles(map = m, urlTemplate = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+              attribution = "Map data © <a href='https://openstreetmap.org'>OpenStreetMap</a> contributors")
+m <- addProviderTiles(map = m, providers$Esri.WorldTopoMap, group = "Esri")
+m <- addProviderTiles(map = m, providers$Esri.WorldImagery, group = "Esri")
+m <- addProviderTiles(map = m, providers$Stamen.Toner, group = "Stamen")
+m <- addProviderTiles(map = m, providers$Stamen.Terrain, group = "Stamen")
+m <- addLayersControl(map = m, baseGroups = c("OpenStreetMap", "Esri", "Stamen"),
+                      options = layersControlOptions(collapsed = FALSE))
+
+# display the map
+m
+
